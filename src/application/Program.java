cea6e8f4d588dbb330f5ec13c65e02327c2769dd
Willy Gutsche <willy.gutsche@unifesp.br>
2023-01-25 00:16:22 -0300

    Connect with database

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,13 @@
 package application;
 
+import java.sql.Connection;
+
+import database.Database;
+
 public class Program {
+    
     public static void main(String[] args) throws Exception {
-        
+        Connection connection = Database.getConnection();
+        connection.close();
     }
 }
